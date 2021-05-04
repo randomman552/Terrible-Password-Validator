5f6951084e3ab814b7d963b59f9b1c5613ad0c32
George Grainger <GraingerGY@cardiff.ac.uk>
2021-05-04 20:54:56 +0100

    Improve visuals

--- validator.js
+++ validator.js
@@ -1,54 +1,55 @@
 function validate() {
     let password = document.getElementById("password").value;
+    let passwordConfirm = document.getElementById("verifyPassword").value;
+
+    // Check if password matches
+    if (password !== passwordConfirm) {
+        showMessage("Passwords do not match!", "Validation Failed!");
+        return;
+    }
 
     // Check for any symbols
     if (password.match(/[!"£$%^&*()\-_=+\[{\]};:'@#~|\\?/]/)) {
-        showError("Password cannot contain any symbols!");
+        showMessage("Password cannot contain any symbols!", "Validation Failed!");
         return;
     }
 
     // Check if password contains any numbers
     if (password.match(/[0-9]/)) {
-        showError("Password cannot contain any numbers!");
+        showMessage("Password cannot contain any numbers!", "Validation Failed!");
         return;
     }
 
     // Check for capital letters
     if (password.match(/[A-Z]/)) {
-        showError("Password cannot contain capital letters!");
+        showMessage("Password cannot contain capital letters!", "Validation Failed!");
         return;
     }
 
     // Check for length
     if (password.length > 4) {
-        showError("Password cannot be longer than 4 characters!");
+        showMessage("Password cannot be longer than 4 characters!", "Validation Failed!");
         return;
     }
 
     // Check for any letters at all
     if (password.match(/[a-z]/)) {
-        showError("Password cannot contain letters!")
+        showMessage("Password cannot contain letters!", "Validation Failed!");
         return;
     }
 
     // Only if there is nothing left in the box do we show success
-    showSuccess("Password validated!")
+    showMessage("Password validated!", "Validation Successful!");
 }
 
-/**
- * Function to show an error message to the user.
- * For now this just wraps alert, but later it will add a visually appealing error.
- * @param message
- */
-function showError(message) {
-    alert(message);
+function clear() {
+    document.getElementById("password").value = "";
+    document.getElementById("verifyPassword").value = "";
 }
 
-/**
- * Function to communicate success to the user.
- * Currently wraps alert, but will later add a visually appealing message to the screen.
- * @param message
- */
-function showSuccess(message) {
-    alert(message)
+function showMessage(message, title) {
+    const myModal = new bootstrap.Modal(document.getElementById('myModal'))
+    document.getElementById("myModalBody").innerText = message;
+    document.getElementById("myModalLabel").innerText = title;
+    myModal.show();
 }
